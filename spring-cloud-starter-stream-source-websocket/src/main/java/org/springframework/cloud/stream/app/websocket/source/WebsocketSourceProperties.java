6d1bf992740199faf1315f7283e1509afb67ddc3
Krishnaprasad <krishna.prasad@flytxt.com>
2018-01-11 13:13:38 +0530

    rebase from master & review changes
    version change
    Readme change
    Copyright added
    Property for allowedOrigins
    Documentation fix

--- spring-cloud-starter-stream-source-websocket/src/main/java/org/springframework/cloud/stream/app/websocket/source/WebsocketSourceProperties.java
+++ spring-cloud-starter-stream-source-websocket/src/main/java/org/springframework/cloud/stream/app/websocket/source/WebsocketSourceProperties.java
@@ -1,11 +1,28 @@
+/*
+ * Copyright 2014-2018 the original author or authors.
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 package org.springframework.cloud.stream.app.websocket.source;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 /**
- * Command line arguments available for {@ WebsocketSourceConfiguration}
+ * Configuration properties available for {@link WebsocketSourceConfiguration}
  *
- * @author krishnaprasad
+ * @author Krishnaprasad A S
  *
  */
 @ConfigurationProperties("websocket")
@@ -13,11 +30,18 @@ public class WebsocketSourceProperties {
 
 	public static final String DEFAULT_PATH = "/websocket";
 
+	private static final String DEFAULT_ALLOWED_ORIGINS = "*";
+
 	/**
-	 * the path on which a WebsocketSource consumer needs to connect. Default is
+	 * The path on which server WebSocket handler is exposed. Default is
 	 * <tt>/websocket</tt>
 	 */
 	String path = DEFAULT_PATH;
+
+	/**
+	 * The allowed origins. Default is <tt>*</tt>
+	 */
+	String allowedOrigins = DEFAULT_ALLOWED_ORIGINS;
 
 	public String getPath() {
 		return path;
@@ -25,6 +49,14 @@ public class WebsocketSourceProperties {
 
 	public void setPath(String path) {
 		this.path = path;
+	}
+
+	public String getAllowedOrigins() {
+		return allowedOrigins;
+	}
+
+	public void setAllowedOrigins(String allowedOrigins) {
+		this.allowedOrigins = allowedOrigins;
 	}
 
 }
